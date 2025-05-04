31e1a8974044c5045c0abf7d9cca793b3f8ea8e2
Tyler Garner <tylerg1015@gmail.com>
2025-05-03 20:07:38 -0500

    Create ExtractionZoneTrigger.cs

---
+++ ExtractionZoneTrigger.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using MoreMountains.TopDownEngine;
+using MoreMountains.Tools;
+using System.Collections;
+
+public class ExtractionZoneTrigger : MonoBehaviour
+{
+    public float extractionTime = 3f;
+    private bool playerInZone = false;
+    private float timer = 0f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInZone = true;
+            timer = 0f;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInZone = false;
+            timer = 0f;
+        }
+    }
+
+    void Update()
+    {
+        if (playerInZone)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= extractionTime)
+            {
+                // Successful extraction!
+                ExtractPlayer();
+            }
+        }
+    }
+
+    void ExtractPlayer()
+    {
+        // Optional: Play feedbacks
+        MMGameEvent.Trigger("ExtractionComplete");
+
+        // Transition to next scene, show UI, etc.
+        TopDownEngineEvent.Trigger(TopDownEngineEventTypes.LevelComplete);
+    }
+}
