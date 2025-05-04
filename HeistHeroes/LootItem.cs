0cfa3da3289e5b2615fb51abf5039063576d0b89
Tyler Garner <tylerg1015@gmail.com>
2025-05-04 07:52:17 -0500

    Create LootItem.cs

---
+++ HeistHeroes/LootItem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class LootItem
+{
+    public string baseName;
+    public Sprite icon;
+    public Rarity rarity;
+    public ItemType type;
+    public List<AffixData> affixes;
+}
